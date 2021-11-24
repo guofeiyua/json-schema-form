d968681682fb2f4a1818a104eb87727fb443364d
guoyanbin <guoyanbin@megvii.com>
2021-11-24 16:37:19 +0800

    feat: format test

--- schema-tests/test.js
+++ schema-tests/test.js
@@ -1,21 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const Ajv = require("ajv");
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
-
+// 自定义format
+ajv.addFormat('test', (data) => {
+  console.log(data)
+  return data === 'hello'
+})
 const schema = {
   type: "object",
   properties: {
     foo: { type: "number" },
-    name: { type: "string", minLength: 3 },
+    name: {
+      type: "string",
+      format: "test",
+    },
     animals: {
-      type: 'array',
+      type: "array",
       items: {
-        type: 'string'
-      }
+        type: "string",
+      },
     },
     isMan: {
-      type: 'boolean'
-    }
+      type: "boolean",
+    },
   },
   required: ["foo", "name"],
   additionalProperties: false,
@@ -25,9 +32,9 @@ const validate = ajv.compile(schema);
 
 const data = {
   foo: 123,
-  name: "abc",
-  animals: ['mao', 'dog'],
-  isMan: true
+  name: "hello",
+  animals: ["mao", "dog"],
+  isMan: true,
 };
 
 const valid = validate(data);
